50c6505a021d1b9b4dbe75fa9408604e6009e49d
Przemo Grzymislawski <przemyslaw.grzymislawski@gmail.com>
2021-01-09 23:36:01 +0100

    Create scrapping NACA values from webpage

--- NACA_reader.py
+++ NACA_reader.py
@@ -1,5 +1,29 @@
 import numpy as np
+from bs4 import BeautifulSoup
+import requests
 
+r = requests.get('http://airfoiltools.com/polar/details?polar=xf-n0009sm-il-1000000')
+soup = BeautifulSoup(r.text, "html.parser") #Parsowanie strony
+table = soup.find('table', attrs={'class': 'tabdata'}) #znalezienie odpowiedniej tabeli,
+                                                        #po uprzednim sprawdzeniu nazwy tabeli w źródle strony
+no_rows = table.find_all('tr')
+#print ('Row number:', len(no_rows)-1) #Wyświetla liczbę wierszy, umniejszoną o wiersz nagłówkowy
+
+cd_cl_data = np.zeros((len(no_rows), 3)) #Tworzy tablicę o żądanym rozmiarze
+#print (cd_cl_data)
+#for i, row in enumerate(no_rows):
+for row in no_rows:
+    data = row.find_all('td') #znalezienie kolumn
+    #for i in data:
+    #    print(i.getText())
+    # cd_cl_data[i][0] = data[0].getText()
+    # cd_cl_data[i][1] = data[1].getText()
+    # cd_cl_data[i][2] = data[2].getText()
+    print(data)
+    #print(len(data))
+    #print(data[0])
+
+#print(cd_cl_data)
 
 def cd_cl(NACA, angle):
     naca = np.loadtxt('./NACA_values/NACA_'+str(NACA)+'.txt', skiprows=12, usecols=(0,1,2))
@@ -37,4 +61,4 @@ def cd_cl(NACA, angle):
     #print(naca[x[0][0]][2])
     
 
-print(cd_cl('0009', -18))
+#print(cd_cl('0009', -18))
