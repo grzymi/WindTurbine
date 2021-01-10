399eb25b62e09848bc14cbd521f9806bbd5c2342
grzymi <przemyslaw.grzymislawski@gmail.com>
2021-01-10 23:46:02 +0100

    Read data from webpage

--- NACA_reader.py
+++ NACA_reader.py
@@ -2,64 +2,37 @@ import numpy as np
 from bs4 import BeautifulSoup
 import requests
 
-r = requests.get('http://airfoiltools.com/polar/details?polar=xf-n0009sm-il-1000000')
-soup = BeautifulSoup(r.text, "html.parser") #Parsowanie strony
-table = soup.find('table', attrs={'class': 'tabdata'}) #znalezienie odpowiedniej tabeli,
-                                                        #po uprzednim sprawdzeniu nazwy tabeli w źródle strony
-no_rows = table.find_all('tr')
-#print ('Row number:', len(no_rows)-1) #Wyświetla liczbę wierszy, umniejszoną o wiersz nagłówkowy
-
-cd_cl_data = np.zeros((len(no_rows), 3)) #Tworzy tablicę o żądanym rozmiarze
-#print (cd_cl_data)
-for i, row in enumerate(no_rows):
-#for row in no_rows:
-    data = row.find_all('td') #znalezienie kolumn
-    if len(data)==0:
-        continue   #for i in data:
-    #    print(i.getText())
-    cd_cl_data[i][0] = data[0].getText()
-    cd_cl_data[i][1] = data[1].getText()
-    cd_cl_data[i][2] = data[2].getText()
-    #print(data)
-    #print(len(data))
-    #print(data[0])
-
-print(cd_cl_data)
-
 def cd_cl(NACA, angle):
-    naca = np.loadtxt('./NACA_values/NACA_'+str(NACA)+'.txt', skiprows=12, usecols=(0,1,2))
-    maximum = naca[-1][0]
-    minimum = naca[0][0]
-    angles = naca[0:-1,0]
-    indices = np.abs(np.subtract.outer(angles, angle)).argmin(0)
-    print (angles[indices])
-    #print (naca[0:-1,0])
+    '''
+    NACA - put the website with profile from airfoiltools.com
+    Examples:
+        http://airfoiltools.com/polar/details?polar=xf-naca0012h-sa-1000000
+        http://airfoiltools.com/polar/details?polar=xf-naca0006-il-1000000
+    '''
+    r = requests.get(NACA)
+    soup = BeautifulSoup(r.text, "html.parser") #Parsowanie strony
+    table = soup.find('table', attrs={'class': 'tabdata'}) #znalezienie odpowiedniej tabeli,
+                                                            #po uprzednim sprawdzeniu nazwy tabeli w źródle strony
+    no_rows = table.find_all('tr')
+    #print ('Row number:', len(no_rows)-1) #Wyświetla liczbę wierszy, umniejszoną o wiersz nagłówkowy
 
-    # if angle>minimum and angle<maximum:
+    cd_cl_data = np.zeros((len(no_rows), 3)) #Tworzy tablicę o żądanym rozmiarze
 
-
-    # try:
-    #     x=np.where(naca==angle)
-    #     Cd = naca[x[0][0]][2]
-    #     Cl = naca[x[0][0]][1]
-    #     return (Cd, Cl)
-    # except:
-    #     if angle < 0:
-    #         x=np.where(naca==minimum)
-    #         Cd = naca[x[0][0]][2]
-    #         Cl = naca[x[0][0]][1]
-    #         print ('Angle is out of range. The minimum value was taken:', minimum)
-    #         return (Cd, Cl)
-    #     else:
-    #         x=np.where(naca==maximum)
-    #         Cd = naca[x[0][0]][2]
-    #         Cl = naca[x[0][0]][1]
-    #         print ('Angle is out of range. The maximum value was taken:', maximum)
-    #         return (Cd, Cl)            
-    #print(x[0][0])
-    # Cd -> index 2
-    # Cl -> index 1
-    #print(naca[x[0][0]][2])
+    for i, row in enumerate(no_rows):
+        data = row.find_all('td') #znalezienie kolumn
+        if len(data)==0:
+            continue   #for i in data:
+        cd_cl_data[i][0] = data[0].getText()
+        cd_cl_data[i][1] = data[1].getText()
+        cd_cl_data[i][2] = data[2].getText()
     
+    cd_cl_data = np.delete(cd_cl_data, 0, 0) #Usunięcie pierwszego wiersza z tablicy
+    angles = cd_cl_data[0:-1,0]
+    indices = np.abs(np.subtract.outer(angles, angle)).argmin(0)
+    x=np.where(cd_cl_data==cd_cl_data[indices][0])
+    Cd = cd_cl_data[x[0][0]][2]
+    Cl = cd_cl_data[x[0][0]][1]
+    return (Cd, Cl)
 
-#print(cd_cl('0009', -18))
+print (cd_cl('http://airfoiltools.com/polar/details?polar=xf-naca0006-il-1000000', 8))
+
